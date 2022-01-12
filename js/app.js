a6c088ff5ac7127b804c31aea5126e8938d67a1d
zaidschouwey98 <zazar981@hotmail.com>
2022-01-12 09:27:10 +0100

    Add animation progression on scroll

--- js/app.js
+++ js/app.js
@@ -27,6 +27,11 @@ tl
         duration: 300,
         offset: '-=875',
         delay: (el, i, l) => 80 * (l - i)
+    }).add({
+        targets: '.scroll-link',
+        opacity: 1,
+        easing: "easeOutExpo",
+        duration: 300
     })
 
 
@@ -41,6 +46,10 @@ new ScrollMagic.Scene({
         textWrapper2.innerHTML = textWrapper2.textContent.replace(/\S/g, "<span class='letter2'>$&</span>");
 
         anime.timeline()
+            .add({
+                targets:'.text-wrapper2',
+                opacity:1
+            })
             .add({
                 targets: '.ml2 .letter2',
                 scale: [0.3, 1],
@@ -58,34 +67,121 @@ new ScrollMagic.Scene({
                 offset: '-=875',
                 delay: (el, i, l) => 80 * (l - i)
             })
-        anime({
-            targets: '#progress-php',
-            width: '80%',
-            easing: 'linear',
-        });
-        anime({
+    })
+    .addIndicators()
+    .addTo(controller);
+
+
+
+new ScrollMagic.Scene({
+    triggerElement: "#js-div",
+    triggerHook: 0.8,
+    duration: "80%",
+    offset: 50
+}).on("enter", () => {
+
+    anime.timeline()
+        .add({
+            targets: '#js-div',
+            opacity: 1,
+            easing: "easeOutExpo",
+            duration: 300
+        })
+        .add({
             targets: '#progress-js',
             width: '75%',
             easing: 'linear',
         });
-        anime({
+}).addIndicators()
+    .addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: "#php-div",
+    triggerHook: 0.8,
+    duration: "80%",
+    offset: 50
+}).on("enter", () => {
+
+    anime.timeline()
+        .add({
+            targets: '#php-div',
+            opacity: 1,
+            easing: "easeOutExpo",
+            duration: 300
+        })
+        .add({
+            targets: '#progress-php',
+            width: '80%',
+            easing: 'linear',
+        });
+}).addIndicators()
+    .addTo(controller);
+
+new ScrollMagic.Scene({
+    triggerElement: "#ruby-div",
+    triggerHook: 0.8,
+    duration: "80%",
+    offset: 50
+}).on("enter", () => {
+
+    anime.timeline()
+        .add({
+            targets: '#ruby-div',
+            opacity: 1,
+            easing: "easeOutExpo",
+            duration: 300
+        })
+        .add({
             targets: '#progress-ruby',
             width: '50%',
             easing: 'linear',
         });
-        anime({
-            targets: '#progress-unity',
+}).addIndicators()
+    .addTo(controller);
+
+    new ScrollMagic.Scene({
+        triggerElement: "#unity-div",
+        triggerHook: 0.8,
+        duration: "80%",
+        offset: 50
+    }).on("enter", () => {
+    
+        anime.timeline()
+            .add({
+                targets: '#unity-div',
+                opacity: 1,
+                easing: "easeOutExpo",
+                duration: 300
+            })
+            .add({
+                targets: '#progress-unity',
             width: '72%',
             easing: 'linear',
-        });
-        anime({
-            targets: '#progress-react',
+            });
+    }).addIndicators()
+        .addTo(controller);
+    
+        new ScrollMagic.Scene({
+            triggerElement: "#react-div",
+            triggerHook: 0.8,
+            duration: "80%",
+            offset: 50
+        }).on("enter", () => {
+        
+            anime.timeline()
+                .add({
+                    targets: '#react-div',
+                    opacity: 1,
+                    easing: "easeOutExpo",
+                    duration: 300
+                })
+                .add({
+                    targets: '#progress-react',
             width: '83%',
             easing: 'linear',
-        });
-    })
-    .addIndicators()
-    .addTo(controller);
+                });
+        }).addIndicators()
+            .addTo(controller);
 
 
 let bg3 = document.getElementById("blog")
@@ -94,17 +190,17 @@ console.log(bg3)
 
 var pathEls = document.querySelectorAll('path');
 for (var i = 0; i < pathEls.length; i++) {
-  var pathEl = pathEls[i];
-  var offset = anime.setDashoffset(pathEl);
-  pathEl.setAttribute('stroke-dashoffset', offset);
-  anime({
-    targets: pathEl,
-    strokeDashoffset: [offset, 0],
-    duration: anime.random(1000, 3000),
-    delay: anime.random(0, 2000),
-    loop: true,
-    direction: 'alternate',
-    easing: 'easeInOutSine',
-    autoplay: true
-  });
+    var pathEl = pathEls[i];
+    var offset = anime.setDashoffset(pathEl);
+    pathEl.setAttribute('stroke-dashoffset', offset);
+    anime({
+        targets: pathEl,
+        strokeDashoffset: [offset, 0],
+        duration: anime.random(1000, 3000),
+        delay: anime.random(0, 2000),
+        loop: true,
+        direction: 'alternate',
+        easing: 'easeInOutSine',
+        autoplay: true
+    });
 }
