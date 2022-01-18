90bee8f2c446bde22348a7d454ae2986e2c713fe
Schouwey <zazar981@hotmail.com>
2022-01-18 15:10:12 +0100

    Svg animation end

--- js/app.js
+++ js/app.js
@@ -248,41 +248,171 @@ new ScrollMagic.Scene({
 
 
 
+    class ProjectAnimation {
+        constructor(controller) {
+            this.controller = controller
+            this.shapes = [
+                {
+                    value: "M0 0H13C13 0 18 112.117 18 184C18 255.883 13 368 13 368H0V0Z"
+                },
+                {
+                    value: "M0 0.999983H50C50 0.999983 195 -5 217 70C239 145 50 378 50 378H0V0.999983Z"
+                },
+                {
+                    value: "M0 1.00003H50C50 1.00003 478 -3.99998 372 147C266 298 50 378 50 378H0V1.00003Z"
+                },
+                {
+                    value: "M0 3.05176e-05H317C317 3.05176e-05 552 52 446 203C340 354 200 377 200 377H0V3.05176e-05Z"
+                },
+                {
+                    value: "M0 0H559.156C559.156 0 580 112.117 580 184C580 255.883 559.156 368 559.156 368H0V0Z"
+                }
+            ]
+            this.opacity = [
+                { value: 0, easing: "easeOutSine", duration: 200 },
+                { value: 1, easing: "easeInOutQuad", duration: 400 }
+            ]
+            this.filter = [
+                { value: "blur(5px) grayscale(100%)", easing: "easeOutSine", duration: 300 },
+                { value: "blur(0px) grayscale(0%)", easing: "easeInOutQuad", duration: 500 }
+            ]
+        }
+        hideBlock(className){
+            anime({
+                targets: className,
+                easing: "easeInQuad",
+                opacity: 0,
+                duration: 200,
+            })
+        }
+        draw(triggerElement, svg, title, content,image) {
+            let titleDrawn = false;
+            let contentDrawn = false;
+            let imageDrawn = false;
 
-let shapes = [
-    {
-        value:"M0 0H13C13 0 18 112.117 18 184C18 255.883 13 368 13 368H0V0Z"
-    },
-    {
-        value: "M0 0.999983H50C50 0.999983 195 -5 217 70C239 145 50 378 50 378H0V0.999983Z"
-    },
-    {
-        value: "M0 1.00003H50C50 1.00003 478 -3.99998 372 147C266 298 50 378 50 378H0V1.00003Z"
-    },
-    {
-        value: "M0 3.05176e-05H317C317 3.05176e-05 552 52 446 203C340 354 200 377 200 377H0V3.05176e-05Z"
-    },
-    {
-        value: "M0 0H559.156C559.156 0 580 112.117 580 184C580 255.883 559.156 368 559.156 368H0V0Z"
+            new ScrollMagic.Scene({
+                triggerElement: triggerElement,
+                triggerHook: 1,
+                duration: "80%",
+                offset: 50
+            }).on("progress", (scroll) => {
+                console.log(Math.round(scroll.progress * (this.shapes.length - 1)) , titleDrawn)
+                let projectTimeline = anime.timeline().add({
+                    targets: svg,
+                    d: this.shapes[Math.round(scroll.progress * (this.shapes.length - 1))],
+                    easing: 'easeOutQuad',
+                    duration: 700,
+                });
+                if (Math.round(scroll.progress * (this.shapes.length - 1)) >= 1 && !titleDrawn)
+                {
+                    titleDrawn = true;
+                    projectTimeline.add({
+                        targets: title,
+                        easing: "easeInQuad",      
+                        opacity: this.opacity,
+                        filter: this.filter,
+                        duration: 1000,
+                    })
+                }
+                if (Math.round(scroll.progress * (this.shapes.length - 1)) >= 2 && !contentDrawn)
+                {
+                    contentDrawn = true;
+                    projectTimeline.add({
+                        targets: content,
+                        easing: "easeInQuad",
+                        opacity: this.opacity,
+                        filter: this.filter,
+                        duration: 1000,
+                    })
+                }
+                if (Math.round(scroll.progress * (this.shapes.length - 1)) >= 4 && !imageDrawn)
+                {
+                    console.log("test")
+                    imageDrawn = true;
+                    projectTimeline.add({
+                        targets: image,
+                        easing: "easeInQuad",
+                        opacity: this.opacity,
+                        filter: this.filter,
+                        duration: 1000,
+                    })
+                }
+
+
+                if(titleDrawn && Math.round(scroll.progress * (this.shapes.length - 1)) < 1)
+                {
+                    this.hideBlock(title)
+                    titleDrawn = false;
+                }
+                if(contentDrawn && Math.round(scroll.progress * (this.shapes.length - 1)) < 2)
+                {
+                    this.hideBlock(content)
+                    contentDrawn = false;
+                }
+                if(imageDrawn && Math.round(scroll.progress * (this.shapes.length - 1)) < 4)
+                {
+                    this.hideBlock(image)
+                    imageDrawn = false;
+                }
+
+
+            })
+            .addTo(this.controller);
+        }
     }
-]
 
 
 
 
-new ScrollMagic.Scene({
-    triggerElement: ".chess-project",
-    triggerHook: 0.9,
-    duration: "80%",
-    offset: 50
-}).on("progress", (scroll) => {
-    console.log(Math.round(scroll.progress*shapes.length))
-    anime({
-        targets: '.project-svg',
-        d: shapes[Math.round(scroll.progress*shapes.length)],
-        easing: 'easeOutQuad',
-        duration: 2000,
-    });
-})
-    .addIndicators()
-    .addTo(controller);
+
+
+
+
+
+let project = new ProjectAnimation(controller);
+project.draw(".chess-project",".chess-svg",".chess-title",".chess-content",".chess-image");
+project.draw(".snakejs-project",".snake-svg",".snake-title",".snake-content",".snake-image");
+project.draw(".beatthemall-project",".beatthemall-svg",".beatthemall-title",".beatthemall-content",".beatthemall-image");
+
+
+
+
+
+// new ScrollMagic.Scene({
+//     triggerElement: ".snakeai-project",
+//     triggerHook: 1,
+//     duration: "80%",
+//     offset: 50
+// }).on("progress", (scroll) => {
+//     anime({
+//         targets: '.snakeai-svg',
+//         d: shapes[Math.round(scroll.progress * (shapes.length - 1))],
+//         easing: 'easeOutQuad',
+//         duration: 2000,
+//     });
+// })
+//     .addIndicators()
+//     .addTo(controller);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
